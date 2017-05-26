7317ceef2253ead22e9e4f474a257e38df507adf
Yuming JIANG <cjcjiang@gmail.com>
2017-05-26 21:56:43 +1000

    UpdateResult.js bug found

--- public/js/UpdateResult.js
+++ public/js/UpdateResult.js
@@ -5,7 +5,10 @@ $(document).ready(function(){
     $.get( "/showUpdateResult",parameters, function(result){
         console.log(result);
         $('#update_result').html(result);
+        $('#continue_ref').html('<a href="/a" id="update_show">show individual result</a>');
     });
+
+    // TODO: $("#update_show") does not work, try use another load
 
     $("#update_show").click(function(event){
         event.preventDefault();
